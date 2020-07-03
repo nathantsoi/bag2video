a0332e49be58206c1db6a908a8f90c263890bc97
Aaron Marburg <amarburg@uw.edu>
2020-07-02 21:29:17 -0700

    Initial commit of my hacked version.

--- bag2video_py3.py
+++ bag2video_py3.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 from __future__ import division
 import rosbag, rospy, numpy as np
 import sys, os, cv2, glob
-from itertools import izip, repeat
+from itertools import repeat
 import argparse
 
 import imageio
+
+# pip install --ignore-installed  --extra-index-url https://rospypi.github.io/simple/ rospy rosbag roslz4 cv_bridge sensor_msgs geometry_msgs
 
 # try to find cv_bridge:
 try:
@@ -18,17 +20,18 @@ except ImportError:
         import roslib; roslib.load_manifest("bag2video")
         from cv_bridge import CvBridge
     except:
-        print "Could not find ROS package: cv_bridge"
-        print "If ROS version is pre-Groovy, try putting this package in ROS_PACKAGE_PATH"
+        print("Could not find ROS package: cv_bridge")
+        print("If ROS version is pre-Groovy, try putting this package in ROS_PACKAGE_PATH")
         sys.exit(1)
 
-def get_info(bag, topic=None, start_time=rospy.Time(0), stop_time=rospy.Time(sys.maxint)):
+def get_info(bag, topic=None, start_time=rospy.Time(0), stop_time=rospy.Time(sys.maxsize)):
     size = (0,0)
     times = []
 
     # read the first message to get the image size
-    msg = bag.read_messages(topics=topic).next()[1]
-    size = (msg.width, msg.height)
+    g = bag.read_messages(topics=topic)
+    _, f, _ = next(g)
+    size = (f.width, f.height)
 
     with open("times.txt", 'w') as fp:
 
@@ -50,15 +53,15 @@ def calc_n_frames(times, precision=10):
     intervals = np.diff(times)
     return np.int64(np.round(precision*intervals/min(intervals)))
 
-def write_frames(bag, writer, total, topic=None, nframes=repeat(1), start_time=rospy.Time(0), stop_time=rospy.Time(sys.maxint), viz=False, encoding='bgr8'):
+def write_frames(bag, writer, total, topic=None, nframes=repeat(1), start_time=rospy.Time(0), stop_time=rospy.Time(sys.maxsize), viz=False, encoding='bgr8'):
     bridge = CvBridge()
     if viz:
         cv2.namedWindow('win')
     count = 1
     iterator = bag.read_messages(topics=topic, start_time=start_time, end_time=stop_time)
-    for (topic, msg, time), reps in izip(iterator, nframes):
-        print '\rWriting frame %s of %s at time %s' % (count, total, time),
-        img = np.asarray(bridge.imgmsg_to_cv2(msg, 'bgr8'))
+    for (topic, msg, time), reps in zip(iterator, nframes):
+        print('Writing frame %s of %s at time %s' % (count, total, time))
+        img = np.asarray(bridge.imgmsg_to_cv2(msg, encoding))
         for rep in range(reps):
             writer.write(img)
         imshow('win', img)
@@ -81,7 +84,7 @@ if __name__ == '__main__':
     parser.add_argument('--viz', '-v', action='store_true', help='Display frames in a GUI window.')
     parser.add_argument('--start', '-s', action='store', default=rospy.Time(0), type=rospy.Time,
                         help='Rostime representing where to start in the bag.')
-    parser.add_argument('--end', '-e', action='store', default=rospy.Time(sys.maxint), type=rospy.Time,
+    parser.add_argument('--end', '-e', action='store', default=rospy.Time(sys.maxsize), type=rospy.Time,
                         help='Rostime representing where to stop in the bag.')
 
     parser.add_argument('--encoding', choices=('rgb8', 'bgr8', 'mono8'), default='bgr8',
@@ -100,12 +103,12 @@ if __name__ == '__main__':
         imshow = noshow
 
     for bagfile in glob.glob(args.bagfile):
-        print bagfile
+        print( bagfile )
         outfile = args.outfile
         if not outfile:
             outfile = os.path.join(*os.path.split(bagfile)[-1].split('.')[:-1]) + '.mp4'
         bag = rosbag.Bag(bagfile, 'r')
-        print 'Calculating video properties'
+        print('Calculating video properties')
 
         rate, minrate, maxrate, size, times = get_info(bag, args.topic, start_time=args.start, stop_time=args.end)
 
@@ -119,16 +122,15 @@ if __name__ == '__main__':
 
             bridge = CvBridge()
 
-
             count = 1
-            nfrmes = repeat(1)
+            nframes = repeat(1)
 
-            iterator = bag.read_messages(topics=topic, start_time=start_time, end_time=stop_time)
+            iterator = bag.read_messages(topics=args.topic, start_time=args.start, end_time=args.end)
 
-            for (topic, msg, time), reps in izip(iterator, nframes):
+            for (topic, msg, time), reps in zip(iterator, nframes):
 
-                print '\rWriting frame %s of %s at time %s' % (count, total, time),
-                img = np.asarray(bridge.imgmsg_to_cv2(msg, 'bgr8'))
+                print ('Writing frame %s of %s at time %s' % (count, len(times), time))
+                img = np.asarray(bridge.imgmsg_to_cv2(msg, 'rgb8'))
 
                 for rep in range(reps):
                     w.append_data(img)
@@ -154,4 +156,4 @@ if __name__ == '__main__':
             write_frames(bag, writer, len(times), topic=args.topic, nframes=nframes, start_time=args.start, stop_time=args.end, encoding=args.encoding)
             writer.release()
 
-        print '\n'
+        print('\n')
