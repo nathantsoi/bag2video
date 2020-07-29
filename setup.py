0fdf4f2bc54696a0158abb58af2dcb53643fa9e0
Aaron Marburg <amarburg@uw.edu>
2020-07-29 11:29:58 -0700

    First step towards making this a catkin'able package.

--- setup.py
+++ setup.py
@@ -1,28 +1,11 @@
-import setuptools
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
 
-setuptools.setup(
-    name="bag2video", # Replace with your own username
-    version="0.0.1",
-    author="Aaron Marburg",
-    author_email="amarburg@uw.edu",
-    description="Scripts to process ROS bagfiles into videos",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/apl-ocean-engineering/bag2video",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    scripts=['bag2video.py', 'bag2images.py']
-    # entry_points={
-    # 'console_scripts': [
-    #     'sample=sample:main',
-    # ],
-    # },
-)
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['bag2video'],
+    package_dir={'': 'src'})
+
+setup(**setup_args)
